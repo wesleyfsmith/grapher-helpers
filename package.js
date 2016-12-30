2272a117251e1576803032f2a32e0bfd11b25665
Wesley Smith <wesleyfsmith@gmail.com>
2016-12-30 10:32:07 -0700

    Bumps version number

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'wesleyfsmith:grapher-helpers',
-  version: '1.0.3',
+  version: '1.0.4',
   // Brief, one-line summary of the package.
   summary: 'Collection and document helpers for common grapher commands.',
   // URL to the Git repository containing the source code for this package.
