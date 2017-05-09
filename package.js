9639283eaa51a87b22d14734f9041b55bc3fb290
Wesley Smith <wesleysmith@Wesleys-MacBook-Pro.local>
2017-05-09 14:00:25 -0700

    bump version number

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'wesleyfsmith:grapher-helpers',
-  version: '1.0.4',
+  version: '1.0.5',
   // Brief, one-line summary of the package.
   summary: 'Collection and document helpers for common grapher commands.',
   // URL to the Git repository containing the source code for this package.
